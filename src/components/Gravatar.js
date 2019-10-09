98143ad6c77977c75f74502b221352d805ef38a7
Abraham <oscara1706cl@gmail.com>
2019-10-09 16:58:16 -0500

    Regresando a master

---
+++ src/components/Gravatar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import md5 from 'md5';
+
+
+function Gravatar(props){
+  const email = props.email
+  const image= props.image
+
+  if (email) {
+
+    const hash= md5(email)
+    return (<img className={props.className} src={`https://www.gravatar.com/avatar/${hash}?d=identicon`} alt="Logo usuario"/>)
+
+  }
+    return( <img className={props.className} src={image} alt="Logo usuario"/>)
+
+
+}
+
+export default Gravatar
