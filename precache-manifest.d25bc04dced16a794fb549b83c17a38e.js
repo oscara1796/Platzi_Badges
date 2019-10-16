79308530c2362914a223a478a9b66949689938db
Abraham <oscara1706cl@gmail.com>
2019-10-16 16:30:57 -0500

    Updates

---
+++ precache-manifest.d25bc04dced16a794fb549b83c17a38e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe1f37c2268e2f1a9c2",
+    "url": "/Platzi_Badges/static/css/main.5c85b7a5.chunk.css"
+  },
+  {
+    "revision": "3fe1f37c2268e2f1a9c2",
+    "url": "/Platzi_Badges/static/js/main.3fe1f37c.chunk.js"
+  },
+  {
+    "revision": "7567608adbdde7d50e75",
+    "url": "/Platzi_Badges/static/js/runtime~main.7567608a.js"
+  },
+  {
+    "revision": "775595a525d2dae43f1c",
+    "url": "/Platzi_Badges/static/css/2.5e0dfab6.chunk.css"
+  },
+  {
+    "revision": "775595a525d2dae43f1c",
+    "url": "/Platzi_Badges/static/js/2.775595a5.chunk.js"
+  },
+  {
+    "revision": "6769eb7c5e14f194211630a788bfecb4",
+    "url": "/Platzi_Badges/static/media/confLogo.6769eb7c.png"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/Platzi_Badges/static/media/badgenew-header.a1880fbf.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Platzi_Badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Platzi_Badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Platzi_Badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "c509a4c46008514b5a5084d2596c99e4",
+    "url": "/Platzi_Badges/static/media/facebook.c509a4c4.svg"
+  },
+  {
+    "revision": "fc52704b9f4f4c859a24777d05ddc737",
+    "url": "/Platzi_Badges/static/media/hero_img.fc52704b.png"
+  },
+  {
+    "revision": "fbd3a5c886b7fba39b2b9e6a8c36fba8",
+    "url": "/Platzi_Badges/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Platzi_Badges/precache-manifest.d25bc04dced16a794fb549b83c17a38e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Platzi_Badges/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
